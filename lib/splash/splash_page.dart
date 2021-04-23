63bd3ed2b8d88b2046d083125bd0b86a1d5d01b4
Rafael Guimarães <rafaelsanct@gmail.com>
2021-04-23 08:54:11 -0300

    [FIX] FIxing the function to open home page in splash page

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -7,7 +7,7 @@ import 'package:DevQuiz/modules/app/pages/home/home_page.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.push(
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => HomePage())));
     return Scaffold(
       body: Container(
